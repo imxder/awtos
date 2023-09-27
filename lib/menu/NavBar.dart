0202267c7874a2f73ddf74994c2298de53e1ab05
Alexandre Salgado <alexandre.salgado@catolicasc.edu.br>
2023-09-27 16:08:32 -0300

    finalizando telas

--- lib/menu/NavBar.dart
+++ lib/menu/NavBar.dart
@@ -1,3 +1,4 @@
+import 'package:awtos/login/login.view.dart';
 import 'package:flutter/material.dart';
 
 class NavBar extends StatefulWidget {
@@ -8,12 +9,81 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  bool isDarkModeEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    return const  Drawer(
-  
-      
-    );  
-    
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            color:  const Color.fromRGBO(1, 28, 105, 0.9), // Cor da barra
+            height: 115, // Altura da barra
+          ),
+          SwitchListTile(
+            title: const  Text('Dark Mode',
+            textAlign: TextAlign.end),
+            value: isDarkModeEnabled,
+            onChanged: (value) {
+              setState(() {
+                isDarkModeEnabled = value;
+                
+              });
+             
+            },
+            
+          ),
+          ListTile(
+            leading: const Icon(Icons.person,
+            color: Color.fromRGBO(1, 28, 105, 0.9)),
+            title: const Text('Perfil'),
+            onTap: () {
+              
+            },
+          ),
+          ListTile(
+            leading:const Icon(Icons.attach_money,
+            color: Color.fromRGBO(1, 28, 105, 0.9)),
+            title: const  Text('Financeiro'),
+            onTap: () {
+              
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.drive_eta,
+            color: Color.fromRGBO(1, 28, 105, 0.9)),
+            title: const Text('Área do Motorista'),
+            onTap: () {
+              
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings,
+            color: Color.fromRGBO(1, 28, 105, 0.9)),
+            title: const Text('Configurações'),
+            onTap: () {
+              
+            },
+          ),
+          const Spacer(), 
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const LoginView(),
+                  ),
+                );
+            },
+            child: const Column(
+              children: [
+                Icon(Icons.logout, size: 25, color: Color.fromRGBO(1, 28, 105, 0.9)), // Ícone de logout
+                Text('Log Out', style: TextStyle(color: Color.fromRGBO(1, 28, 105, 0.9))),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
   }
 }
