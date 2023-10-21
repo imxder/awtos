b956774acb519345bbb69714a434cc9404c38ba3
Alexandre Salgado <imxderr@gmail.com>
2023-10-21 19:33:31 -0300

    FIREBASE SETUP

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:awtos/firebase_options.dart';
 import 'package:awtos/login/login.view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp (const MyApp());
 }
 
